fdd2d2746b77e5bfcd7cb4dd8e654f5c4305e274
clostrid <clostridium@korea.ac.kr>
2023-10-27 17:54:30 +0900

    Revert "Update date_pin.py"
    This reverts commit 93b32f3ebf1b82e3efb18701da354cfee78ead15.

--- date_pin/date_pin.py
+++ date_pin/date_pin.py
@@ -1,107 +1,130 @@
-import binascii
 import os
+from datetime import datetime
 
-
-def guess_ext(filename)->list[str]: # filename:str - 파일 경로
-    enable_list = []
-    
-    # 파일 시그니처와 해당 확장자 매핑
-    # 리스트를 리턴하는 방식으로 구현하여 시그니처가 동일한 다른 확장자에 대한 목록을 리턴
-    header_signatures = {
-        b'\xFF\xD8\xFF': ['jpg/jpeg'],
-        b'\x89\x50\x4E\x47\x0D\x0A\x1A\x0A': ['png'],
-        b'\x47\x49\x46\x38\x37\x61': ['gif'],
-        b'\x47\x49\x46\x38\x39\x61': ['gif'],
-        b'\x42\x4D': ['bmp'],
-        b'\x49\x20\x49': ['tif'],
-        b'\x49\x20\x2A\x00': ['tiff'],
-        b'\x50\x4B\x03\x04': ['zip'],
-        b'\x50\x4B\x05\x06': ['zip'],
-        b'\x50\x4B\x07\x08': ['zip'],
-        b'\x7B\x5C\x72\x74\x66': ['rtf'],
-        b'\x25\x50\x44\x46': ['pdf'],
-        b'\x4D\x5A': ['exe'],
-        b'\x4D\x5A\x90\x00\x03\x00\x00\x00': ['exe'],
-        b'\x4D\x5A\x50\x00\x02\x00\x00\x00': ['exe'],
-        b'\x4D\x5A\x4E\x00\x02\x00\x00\x00': ['exe'],
-        b'\x4D\x5A\x6E\x00\x00\x00\x00\x00': ['exe'],
-        b'\x25\x21\x50\x53\x2D\x41\x64\x6F': ['ps'],
-        b'\x25\x21\x50\x45\x0D\x0A': ['eps'],
-        b'\x1F\x8B\x08': ['gz'],
-        b'\x42\x5A\x68': ['bz2'],
-        b'\x50\x4B\x03\x04': ['zip'],
-        b'\x50\x4B\x05\x06': ['zip'],
-        b'\x50\x4B\x07\x08': ['zip'],
-        b'\x1F\x9D': ['jar'],
-        b'\x4D\x5A\x49\x53': ['cab'],
-        b'\x4D\x44\x4D\x50': ['msi'],
-        b'\x43\x30\x30\x31': ['iso'],
-        b'\x21\x3C\x61\x72\x63\x68\x3E': ['arc'],
-        b'\x1A\x45\xDF\xA3': ['mkv'],
-        b'\x1F\x43\x4F\x4D': ['z'],
-        b'\x75\x73\x74\x61\x72': ['tar'],
-        b'\x4C\x5A\x49\x50': ['lz'],
-        b'\x28\x54\x68\x69\x73\x20\x66\x69\x6C\x65': ['hqx'],
-        # 다른 파일 형식의 헤더 시그니처를 여기에 추가할 수 있습니다.
-    }
-    footer_signature = {
-        b'\xFF\xD9': ['jpg/jpeg'],
-        b'\x00\x3B': ['gif'],
-        b'\x49\x45\x4E\x44\xAE\x42\x60\x82': ['png'],
-        b'\x25\x25\x45\x4F\x46': ['pdf'],
-        b'\x50\x4B\x05\x06': ['zip'],
-        # 다른 파일 형식의 푸터 시그니처를 여기에 추가할 수 있습니다.
-    }
-
+def add_pin(filename:str):
     try:
-        sign_search_len = 30
-        pin = 0
-        f = open(filename, "rb")
-
-        # file size
-        filelen_byte = os.path.getsize(filename)
-
-        '''
-        if has_pin(filename):
-            remove_pin(filename)
-            pin = 1
-        '''
-        
-        # file size 가 작은 경우를 고려
-        if filelen_byte < sign_search_len:
-            sign_search_len = filelen_byte
-        
-        # 파일 초반 바이트를 읽어들여 파일 헤더 시그니처 목록에 매핑하여 가능한 파일 확장자 분류 
-        for i in range(1, sign_search_len):
-            file_signature = f.read(i)
-            enable_value = header_signatures.get(file_signature)
-            if enable_value:
-                enable_list.extend(enable_value)
-            f.seek(0)
-        # 파일 후반 바이트를 읽어들여 파일 푸터 시그니처 목록에 매핑하여 가능한 파일 확장자 분류 (작성 필요)
-        for i in range(1, sign_search_len):
-            f.seek(filelen_byte - i)
-            file_signature = f.read(i)
-            enable_value = footer_signature.get(file_signature)
-            if enable_value:
-                enable_list.extend(enable_value)
-            f.seek(0)
-        f.close()
-
-        '''
-        if pin == 1:
-            add_pin(filename)
-        '''
-
-    except FileNotFoundError: 
+        # 파일 열기
+        with open(filename, 'r+') as file:
+            # 파일 내용 읽기
+            content = file.read()
+            
+            # 파일 내용을 수정하여 상단에 핀 추가
+            modified_content = f'PINNED: {content}'
+            
+            # 파일 포인터를 파일의 시작으로 이동
+            file.seek(0)
+            
+            # 수정된 내용을 파일에 쓰기
+            file.write(modified_content)
+            
+            print(f'{filename}에 핀이 추가되었습니다.')
+    except FileNotFoundError:
         print(f'Error: {filename}을(를) 찾을 수 없습니다.')
     except Exception as e:
         print(f'Error: {e}')
-    
-    return enable_list # list[str] - 가능한 확장자명을 리스트에 담아 출력
 
 
-# 테스팅 코드
-fd = "C:/Users/clost/Downloads/PALOMA.pdf"
-flist = guess_ext(fd)
-print(flist)
+def has_pin_in_directory(directory:str) -> bool:
+    try:
+        # 디렉토리 내 모든 파일 리스트 가져오기
+        files = os.listdir(directory)
+        
+        # 디렉토리 내 파일 순회
+        for file_name in files:
+            # 파일의 전체 경로 생성
+            file_path = os.path.join(directory, file_name)
+            
+            # 파일인지 확인
+            if os.path.isfile(file_path):
+                # 파일 내용 읽기
+                with open(file_path, 'r') as file:
+                    content = file.read()
+                    
+                    # 핀이 있는지 확인
+                    if content.startswith('PINNED: '):
+                        return True
+        return False
+    except Exception as e:
+        print(f'Error: {e}')
+        return False
+
+def get_inspect_date(filename:str) -> datetime:
+    try:
+        # 파일 열기
+        with open(filename, 'r') as file:
+            # 파일 내용 읽기
+            content = file.read()
+            
+            # 'inspect_date' 값을 추출
+            inspect_date_str = content.split('inspect_date: ')[1].split('\n')[0]
+            
+            # 날짜 문자열을 datetime 객체로 변환
+            inspect_date = datetime.strptime(inspect_date_str, '%Y-%m-%d %H:%M:%S')
+            
+            return inspect_date
+    except FileNotFoundError:
+        print(f'Error: {filename}을(를) 찾을 수 없습니다.')
+        return None
+    except Exception as e:
+        print(f'Error: {e}')
+        return None
+
+def has_pin(filename:str) -> bool:
+    try:
+        # 파일 열기
+        with open(filename, 'r') as file:
+            # 파일 내용 읽기
+            content = file.read()
+            
+            # 핀이 있는지 확인
+            return content.startswith('PINNED: ')
+    except FileNotFoundError:
+        print(f'Error: {filename}을(를) 찾을 수 없습니다.')
+        return False
+    except Exception as e:
+        print(f'Error: {e}')
+        return False
+
+
+def remove_pin(filename:str):
+    try:
+        # 파일 열기
+        with open(filename, 'r+') as file:
+            # 파일 내용 읽기
+            content = file.read()
+            
+            # 핀이 있는지 확인하고 있다면 제거
+            if content.startswith('PINNED: '):
+                modified_content = content[len('PINNED: '):]  # "PINNED: " 부분을 제외한 나머지 내용
+                
+                # 파일 포인터를 파일의 시작으로 이동
+                file.seek(0)
+                
+                # 수정된 내용을 파일에 쓰기
+                file.write(modified_content)
+                
+                print(f'{filename}의 핀이 제거되었습니다.')
+            else:
+                print(f'{filename}에는 핀이 존재하지 않습니다.')
+    except FileNotFoundError:
+        print(f'Error: {filename}을(를) 찾을 수 없습니다.')
+    except Exception as e:
+        print(f'Error: {e}')
+
+def set_inspect_date(filename:str, date:datetime):
+    try:
+        # 파일 열기
+        with open(filename, 'a') as file:
+            # 현재 날짜 및 시간을 문자열로 변환하여 파일에 추가
+            file.write(f'\ninspect_date: {date.strftime("%Y-%m-%d %H:%M:%S")}\n')
+            
+        print(f'{filename}에 분석 날짜가 추가되었습니다.')
+    except Exception as e:
+        print(f'Error: {e}')
+
+
+
+
+
+
+
